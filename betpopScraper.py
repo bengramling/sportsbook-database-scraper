65542d6e9fd5e038339bdffa32a08a2d8765cd7e
Ben Gramling <bengram@iu.edu>
2021-05-14 12:42:43 -0400

    works headless

--- betpopScraper.py
+++ betpopScraper.py
@@ -1,8 +1,6 @@
 import os
-from bs4 import BeautifulSoup
 import pandas as pd
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import selenium.common.exceptions as exceptions
 from selenium.webdriver.chrome.options import Options
@@ -12,19 +10,23 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 
 options = Options()
-#options.add_argument("--headless")
+options.add_argument("--headless")
 
 driver = webdriver.Chrome(executable_path=os.path.abspath("/Users/benjamingramling/Desktop/Apps/chromedriver"),
 options=options)
+games = []
 
 #SCRAPING LIVE BASKETBALL
 driver.get('https://plive.ffsvrs.lv/live/?#!/sport/2')
 actions = ActionChains(driver)
 
 def get_games(panel):
-    games = WebDriverWait(panel, timeout=4).until(
+    events = WebDriverWait(panel, timeout=4).until(
         EC.presence_of_all_elements_located((By.XPATH, ".//div[@class='event-list__item']"))
     )
+    games = []
+    for event in events:
+        games.append(event.get_attribute('event-id'))
     return games
 
 def expand_all_panels(driver): 
@@ -39,68 +41,41 @@ def expand_all_panels(driver):
     except exceptions.TimeoutException:
         print("Error: Timeout while attempting to press panel expand button")
         
-
-#clik the game winner
-def click_game_winner(driver, panel):
-    #get name of league
-    league = panel.find_element_by_xpath(".//h3[@class='panel-title']").text
-
-    #click on the game winner option in the drop down list of the odds container
-    try:
-        selector = WebDriverWait(panel, timeout=4).until(
-            EC.presence_of_element_located((By.XPATH, ".//div[@class='dropdown market-selector']"))
-        )
-        try:
-            print("Attempting to move to game winnner element for",league)
-            actions.move_to_element(selector).perform()
-        except:
-            print("Failed to move to game winnner element for",league)
-        toggler = WebDriverWait(selector, timeout=2).until(
-            EC.element_to_be_clickable((By.XPATH, ".//button[@class='market-selector__toggler']"))
-        )
-        actions.move_to_element(toggler).perform()
-        toggler.click()
-        try:
-            game_winner = WebDriverWait(selector, timeout=2).until(
-                EC.element_to_be_clickable((By.XPATH, ".//li[text()='Game Winner']"))
-            )
-            game_winner.click()
-        except exceptions.TimeoutException:
-            print("Error on panel ",league,": Timeout while trying to click GameWinner. Reclicking toggler.")
-            toggler.click()
-    except exceptions.TimeoutException:
-        print("Error on panel ",league,": Timeout while trying to click toggler/selector.")
-        pass
-    except exceptions.WebDriverException:
-        print("Error on panel ",league,": WebdriverException")
-        pass
         
-def find_data(game):
-    #find teams
-    team1 = game.find_element_by_xpath(".//p[@data-testid='top-team-details']").get_attribute('title')
-    team2 = game.find_element_by_xpath(".//p[@data-testid='bottom-team-details']").get_attribute('title')
-    contest = [team1, team2]
-
-    #find odds
+def find_data(driver, game):
+    #get the game winner panel
     try:
-        odds_container = WebDriverWait(game, timeout=4).until(
-            EC.presence_of_element_located(((By.XPATH, ".//div[@class='offerings market--two-row market-3']")))
-        )
-        odds_conts = odds_container.find_elements_by_xpath(".//span[@class='emphasis']")
-        odds = [int(odds_conts[0].text), int(odds_conts[1].text)]
-        if odds[0] > odds[1]:
-            favorite = {team2: odds[1]}
-            underdog = {team1: odds[0]}
-        else:
-            favorite = {team1: odds[0]}
-            underdog = {team2: odds[1]}
-    except:
-        print("Error: Could not get odds for",team1, "vs", team2)
-        favorite = {}
-        underdog = {}
-        pass
+        try:
+            game_winner_panel = WebDriverWait(driver, timeout=6).until(
+                EC.presence_of_element_located(((By.XPATH, "//h3[text() = 'Game Winner']")))
+            )
+        except:
+            print("Error: No game winner panel found for gameID:",game)
+            return {}
 
-    output = pd.Series({'Sport': 'Basketball', 'Contenders': contest, 'Favorite': favorite, 'Underdog': underdog})
+        panel_heading = game_winner_panel.find_element_by_xpath('./..')
+        if panel_heading.get_attribute('class') == 'panel-heading closed':
+            panel_heading.click()
+        
+        panel = panel_heading.find_element_by_xpath('./..')
+        odds_container = panel.find_element_by_xpath(".//div[@class='two-way']")
+        odds_conts = odds_container.find_elements_by_xpath(".//odds[@class='odds ng-isolate-scope']")
+        team0 = odds_conts[0].find_element_by_xpath(".//span[@class= 'ng-binding ng-scope']").text
+        odds0 = int(odds_conts[0].find_element_by_xpath(".//span[@class= 'emphasis ng-binding ng-scope']").text)
+        team1 = odds_conts[1].find_element_by_xpath(".//span[@class= 'ng-binding ng-scope']").text
+        odds1 = int(odds_conts[1].find_element_by_xpath(".//span[@class= 'emphasis ng-binding ng-scope']").text)
+        if odds0 > odds1:
+            favorite = {team1: odds1}
+            underdog = {team0: odds0}
+        else:
+            favorite = {team0: odds0}
+            underdog = {team1: odds1}
+    except Exception as e:
+        print("Error: Could not get data for event",game)
+        print(e)
+        return {}
+
+    output = pd.Series({'Sport': 'Basketball', 'Contenders': [team0, team1], 'Favorite': favorite, 'Underdog': underdog})
     return output
 
 #create dataframe
@@ -121,23 +96,18 @@ try:
 
     #Gather game data for each panel
     for panel in panels:
-        #click game winner
-        click_game_winner(driver, panel)
-
         #get all games in panel
-        try:
-            games = get_games(panel)
-        except:
-            expand_all_panels(driver)
-            try:
-                get_games(panel)
-            except:
-                print("Error: could not get games for this panel")
+        games += get_games(panel)
 
-        #add games to list
-        for game in games:
-            db = db.append(find_data(game), ignore_index = True)
-
+    for game in games:
+        url = 'https://plive.ffsvrs.lv/live/?#!/event/' + game
+        driver.get(url)
+        data = find_data(driver,game)
+        if isinstance(data, pd.Series):
+            db = db.append(data, ignore_index = True)
+            db.to_csv('~/Desktop/output.csv', index = False, header=True)
+        else:
+            pass
+        
 finally:
     driver.quit()
-    db.to_csv('~/Desktop/output.csv', index = False, header=True)
